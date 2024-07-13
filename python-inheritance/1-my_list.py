11275aa709149325d66218aae09b60f772d0f009
audrey <m.audrey1@allustudent.com>
2024-07-13 11:43:23 -0700

    au

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,6 +1,12 @@
-# Define the MyList class
+#!/usr/bin/python3
+""""List inheritance"""
+
+
 class MyList(list):
-    # Method to print the list in sorted order
+    """Class MyList inherits list."""
+
     def print_sorted(self):
-        # Print the sorted list
-        print(sorted(self))
+        """Prints sorted lists."""
+        temp_list = self[:]
+        temp_list.sort()
+        print("{}".format(temp_list))
